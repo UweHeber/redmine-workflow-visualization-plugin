feacd91dccc38ed83491ce7ac4355107ba39f10a
Uwe Heber <uwe@heber.it>
2018-03-17 21:44:03 +0100

    add client logic and style to make workflow tables scrollable, the first column being fixed

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :workflow_visualization do
+    name 'Workflow Visualization plugin'
+    author 'Uwe Heber, Matthias Petermann'
+    description 'This Redmine plugin adjusts the visualization of the workflow administration.'
+    version '0.0.1'
+    url 'https://github.com/UweHeber/'
+    author_url 'http://heber.it/about, http://petermann-it.de'
+end
