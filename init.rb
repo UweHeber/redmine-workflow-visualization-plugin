ded22080ec5c9a73981e8cb1e90ef12e40984062
Uwe Heber <uwe@heber.it>
2018-03-19 20:31:09 +0100

    Solved bug - Changes to the current workflow configuration were not saved. After pressing "Save" and the subsequent reload of the page, the checkboxes were empty or had the old status

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :workflow_visualization do
     name 'Workflow Visualization plugin'
     author 'Uwe Heber, Matthias Petermann'
     description 'This Redmine plugin adjusts the visualization of the workflow administration to make workflow tables scrollable, the first column being fixed.'
-    version '1.0.0'
+    version '1.0.1'
     url 'https://github.com/UweHeber/redmine-workflow-visualization-plugin'
     author_url 'http://heber.it/about, http://petermann-it.de'
 end
